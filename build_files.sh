a6aa69353de42fb35d997ec51e91af9a7bec63e8
Arnab <mannaarnab63@gmail.com>
2023-01-10 14:38:26 +0530

    jeson_data

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 echo " BUILD START"
 
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo " BUILD END"
 
